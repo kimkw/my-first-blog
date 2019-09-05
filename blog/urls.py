e37258234ecc6964d3cab4276a96a4b62a7f6205
kw <freekkw@naver.com>
2019-09-06 02:12:52 +0900

    Add view and template for detailed blog post

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from django.shortcuts import render, get_object_or_404
 
 urlpatterns = [
 	path('', views.post_list, name="post_list"),
+	path('post/<int:pk>/', views.post_detail, name="post_detail"),
 ]
